62476441b46926bf6a075418c1ea74b5c3514874
MHD78 <mdamirchi27@gmail.com>
2021-05-18 20:56:41 +0430

    V1.01

--- webbot.py
+++ webbot.py
@@ -1,1 +1,10 @@
-print("hello world !")
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+mydriver = webdriver.Chrome()
+mydriver.get("https://www.bbc.com/news")
+print(mydriver.title)
+print(mydriver.current_url)
+mydriver.find_element_by_xpath("//*[@id="orb-nav-links"]/ul/li[3]/a").click()
+time.sleep(5)
+mydriver.close()
