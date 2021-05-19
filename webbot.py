994c4c6fda20bc5ae264c9597ae2778a7cfb547b
MHD78 <mdamirchi27@gmail.com>
2021-05-19 18:58:32 +0430

    v1.02

--- webbot.py
+++ webbot.py
@@ -2,9 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 mydriver = webdriver.Chrome()
-mydriver.get("https://www.bbc.com/news")
+mydriver.get("http://www.speedcheck.ir/")
 print(mydriver.title)
 print(mydriver.current_url)
-mydriver.find_element_by_xpath("//*[@id="orb-nav-links"]/ul/li[3]/a").click()
+mydriver.find_element_by_xpath("/html/body/div[2]/div[3]/button[2]").click()
+mydriver.find_element_by_xpath("/html/body/div[2]/div[2]/div/h1").click()
 time.sleep(5)
-mydriver.close()
+# mydriver.back()
+# mydriver.close()
